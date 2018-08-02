5ec418cbc4951240bc3cde56be35ae9837edf042
Dimitar Genov <dimitargenov@Mitkos-MacBook-Air.local>
2018-08-02 14:50:09 +0300

    highchart added

--- package/bstg-source/populationController.js
+++ package/bstg-source/populationController.js
@@ -1,10 +1,63 @@
 (function () {
-
+    var _ = require('underscore');
+    var Highcharts = require('highcharts');
     function SumCitiesPopulation ($scope, appScriptService) {
-
+       
         appScriptService.callAppScript('country/population', {})
         .then(function(response){
             $scope.countries = response;
+
+            var responseToArrays = _(response).each(function(elem, key){
+                response[key] = _(elem).values();
+              });
+           
+            var myChart = new Highcharts.Highcharts.Chart({
+                chart: {
+                    renderTo: 'container',
+                    type: 'column'
+                },
+                title: {
+                    text: 'Countries Population'
+                },
+                xAxis: {
+                    type: 'category',
+                    labels: {
+                        rotation: -45,
+                        style: {
+                            fontSize: '13px',
+                            fontFamily: 'Verdana, sans-serif'
+                        }
+                    }
+                },
+                yAxis: {
+                    min: 0,
+                    title: {
+                        text: 'Population'
+                    }
+                },
+                legend: {
+                    enabled: false
+                },
+                tooltip: {
+                    pointFormat: 'Population: <b>{point.y:.1f} million people</b>'
+                },
+                series: [{
+                    name: 'Population',
+                    data: responseToArrays,
+                    dataLabels: {
+                        enabled: true,
+                        rotation: -90,
+                        color: '#FFFFFF',
+                        align: 'right',
+                        format: '{point.y}', // one decimal
+                        y: 10, // 10 pixels down from the top
+                        style: {
+                            fontSize: '13px',
+                            fontFamily: 'Verdana, sans-serif'
+                        }
+                    }
+                }]
+            });
           });
     }
 
