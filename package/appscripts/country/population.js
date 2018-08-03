6d85549d7b63a68e4d2d6e75d37dca09101ee26d
Dimitar Genov <dimitargenov@Mitkos-MacBook-Air.local>
2018-08-03 14:55:12 +0300

    commit in new branch

--- package/appscripts/country/population.js
+++ package/appscripts/country/population.js
@@ -1,5 +1,6 @@
 function main(ctx, params, done) {
     var _ = require('lib/underscore');
+    console.log('branch vrountry1.1');
     
     ctx.event.db.view('countries', 'population_all_countries', {
         reduce: true,
@@ -22,7 +23,8 @@ function main(ctx, params, done) {
                 var result = _.pluck(res.rows, 'key');
 
                 for (let index = 0; index < result.length; index++) {
-                    var countryFpExtId = _.find(countriesObject, function(country){ return result[index] == country.doc.fp_ext_id; });
+                    var countryFpExtId = _.find(countriesObject, 
+                        function(country){ return result[index] == country.doc.fp_ext_id; });
                     res.rows[index].key = countryFpExtId.doc.name;
                 }
             
